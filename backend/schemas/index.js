2e075b2816e983f388daa510f40d9d3367978665
IamLam163 <olamidealiyu@gmail.com>
2023-07-08 23:54:41 +0100

    added authors

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,3 +1,10 @@
 import testimonials from "./testimonials"
+import about from "./about"
+import brands from "./brands"
+import contacts from "./contacts"
+import experiences from "./experiences"
+import skills from "./skills"
+import workexperience from "./workexperience"
+import works from "./works"
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [about, brands, contacts, experiences, skills, testimonials, workexperience, works]
